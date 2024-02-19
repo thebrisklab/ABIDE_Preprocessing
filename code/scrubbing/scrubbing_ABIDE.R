48fc2e54710eba051d90bef5b3bad8cc2a554931
Benjamin Risk <benjamin.risk@gmail.com>
2024-02-19 09:47:56 -0500

    updated scrubbing_ABIDE.R with most recent cluster version

--- code/scrubbing/scrubbing_ABIDE.R
+++ code/scrubbing/scrubbing_ABIDE.R
@@ -1,7 +1,5 @@
 # This code is used on cluster
 
-args <- commandArgs(TRUE)
-source=args[1]
 
 ########### funciton
 
@@ -43,34 +41,22 @@ scrub <- function(control,tr=2.5,time=5,back=0,forward=0,volume=NULL){
 #/data/home4/risk_share/ImproveFConnASD/ABIDE/fmriprep_preprocessed/ABIDEII-NYU_2/derivatives/sub-29151/ses-1/func/sub-29151_ses-1_task-rest_run-1_desc-confounds_timeseries.tsv
 
 
-
-
-Get_Delta <- function(SUB_ID,source,path,tr){  # we need to set the filename in this function
+### for one subject
+Get_Delta <- function(SITE_ID,SUB_ID,path,tr){  # we need to set the filename in this function
   require(rlang)
-  Delta_fd_lenient=c()
-  Delta_fd_strict=c()
-  Delta_rmsd=c()
+
+  if(SITE_ID %in% c("ABIDEI-KKI","ABIDEI-NYU")){
+    scrub_address <- paste0(path,"fmriprep_preprocessed/",SITE_ID,"/derivatives/",SUB_ID,"/func/",SUB_ID,"_task-rest_run-1_desc-confounds_timeseries.tsv")
+  }else if(SITE_ID== "ABIDEII-NYU_1"){
+    scrub_address <- paste0(path,"fmriprep_preprocessed/",SITE_ID,"/derivatives/",SUB_ID,"/ses-1/func/",SUB_ID,"_ses-1_task-rest_run-1_desc-confounds_timeseries.tsv")
+  }else if(SITE_ID=="ABIDEII-KKI"){
+      chan=read.csv("/home/lwan456/scrubbing_outcome/ABIDE-II_KKI_1_AdditionalScanInfo.csv",header=TRUE)####### may be need change
+      chan_s=chan$RECEIVING_COIL[which(chan$KKI_INDISUB_ID==as.numeric(strsplit(SUB_ID,split = "-")[[1]][[2]]))]
+      channel=strsplit(as.character(chan_s)," ")[[1]][[1]] 
+      scrub_address <- paste0(path,"fmriprep_preprocessed/",SITE_ID,"/derivatives/",SUB_ID,"/ses-1/func/",SUB_ID,"_ses-1_task-rest_acq-rc",channel,"chan_run-1_desc-confounds_timeseries.tsv")
+  }
   
-  Delta_fd_lenient_bf=c()
-  Delta_fd_strict_bf=c()
-  Delta_rmsd_bf=c()
-  
-  Mean_FD=c()
-  Mean_RMSD=c()
-  if(!is_empty(grep(pattern = "ABIDEI-",x = source))){
-      get_address <- function(path,source,i){paste0(path,source,"/derivatives/sub-00",i,"/func/sub-00",i,"_task-rest_run-1_desc-confounds_timeseries.tsv")}
-   }else if(!is_empty(grep(pattern = "ABIDEII-NYU",x = source))){
-      get_address <- function(path,source,i){paste0(path,source,"/derivatives/sub-",i,"/ses-1/func/sub-",i,"_ses-1_task-rest_run-1_desc-confounds_timeseries.tsv")}
-   }else if(source=="ABIDEII-KKI"){
-      get_address <- function(path,source,i){
-        chan=read.csv("/home/lwan456/scrubbing_outcome/ABIDE-II_KKI_1_AdditionalScanInfo.csv",header=TRUE)####### may be need change
-        chan_s=chan$RECEIVING_COIL[which(chan$KKI_INDISUB_ID==i)]
-        channel=strsplit(as.character(chan_s)," ")[[1]][[1]] 
-        paste0(path,source,"/derivatives/sub-",i,"/ses-1/func/sub-",i,"_ses-1_task-rest_acq-rc",channel,"chan_run-1_desc-confounds_timeseries.tsv")
-        }
-    }
-  for (i in SUB_ID) {
-    filename=get_address(path=path,source=source,i=i)
+    filename=scrub_address
     
     SCRUB=read.table(file = filename,header = TRUE,stringsAsFactors = FALSE)[-1,]
     
@@ -78,105 +64,98 @@ Get_Delta <- function(SUB_ID,source,path,tr){  # we need to set the filename in 
     RMSD=as.numeric(SCRUB$rmsd)
     #dvars=as.numeric(SCRUB$std_dvars)
     #####
-    Mean_FD=c(Mean_FD,mean(FD))
-    Mean_RMSD=c(Mean_RMSD,mean(RMSD))
+    Mean_FD=mean(FD)
+    Mean_RMSD=mean(RMSD)
+    propfd02=mean(FD<0.2)
+    propfd05=mean(FD<0.5)
+    propRMSD025=mean(RMSD<0.25)
     #Mean_dvars=c(Mean_dvars,mean(dvars))
     
     ########################################################### Power 2014 FD scrub
     #FD<0.2 strict criteria without back and forward with at least 5 mins as default
-    delta_fd_strict=scrub(control=FD<0.2,tr=tr)
-    Delta_fd_strict=c(Delta_fd_strict,delta_fd_strict)
+    Delta_fd_strict=scrub(control=FD<0.2,tr=tr)
+    
     
     ## Add forward=2 and backward=1 to FD<0.2 strict criteria
-    delta_fd_strict_bf=scrub(control=FD<0.2,tr=tr,back = 1,forward = 2)
-    Delta_fd_strict_bf=c(Delta_fd_strict_bf,delta_fd_strict_bf)
+    Delta_fd_strict_bf=scrub(control=FD<0.2,tr=tr,back = 1,forward = 2)
     
     #FD<0.5 lenient criteria without back and forward
-    delta_fd_lenient=scrub(control=FD<0.5,tr=tr)
-    Delta_fd_lenient=c(Delta_fd_lenient,delta_fd_lenient)
-    
+    Delta_fd_lenient=scrub(control=FD<0.5,tr=tr)
     
     ##  Add forward=2 and backward=1 to FD<0.5 lenient criteria
     
-    delta_fd_lenient_bf=scrub(control=FD<0.5,tr=tr,back = 1,forward = 2)
-    Delta_fd_lenient_bf=c(Delta_fd_lenient_bf,delta_fd_lenient_bf)
-    
-    
-    
+    Delta_fd_lenient_bf=scrub(control=FD<0.5,tr=tr,back = 1,forward = 2)
     
     ########################################################## Power 2014 FD scrub
     
-    
-    
-    
     #### Ciric 2017
     #mean(RMSD)<0.2  && RMSD < 0.25 with at least 5 mins
-    if(mean(RMSD)<0.2&scrub(control=RMSD<0.25,tr=tr)){delta_rmsd=1}else{delta_rmsd=0}
-    Delta_rmsd=c(Delta_rmsd,delta_rmsd)
+    if(mean(RMSD)<0.2&scrub(control=RMSD<0.25,tr=tr)){Delta_rmsd=1}else{Delta_rmsd=0}
     
     ########## add back and forward
-    if(mean(RMSD)<0.2&scrub(control=RMSD<0.25,tr=tr,back = 1,forward = 2)){delta_rmsd_bf=1}else{delta_rmsd_bf=0}
-    Delta_rmsd_bf=c(Delta_rmsd_bf,delta_rmsd_bf)
-    
-    
-    
-    
-    
+    if(mean(RMSD)<0.2&scrub(control=RMSD<0.25,tr=tr,back = 1,forward = 2)){Delta_rmsd_bf=1}else{Delta_rmsd_bf=0}
+
     
     ##### Power 2012
     #FD<0.5 && dvars < 0.5% \delta BOLD
-    #delta_dvars_2012=scrub(control = dvars<1.5&FD<0.5,back = 1,forward = 2)
-    #Delta_dvars_2012=c(Delta_dvars_2012,delta_dvars_2012)
- 
-  }
-  Delta_outcome=data.frame(subject_ID=SUB_ID,Delta_fd_powerpt5=Delta_fd_lenient,Delta_fd_powerpt5_bf=Delta_fd_lenient_bf,Delta_fd_powerpt2=Delta_fd_strict,
-                            Delta_fd_powerpt2_bf=Delta_fd_strict_bf,Delta_ciric=Delta_rmsd,Delta_ciric_bf=Delta_rmsd_bf,Mean_FD=Mean_FD,Mean_RMSD=Mean_RMSD)
+    #Delta_dvars_2012=scrub(control = dvars<1.5&FD<0.5,back = 1,forward = 2)
+    
+  
+  Delta_outcome=data.frame(SUB_ID=SUB_ID,Delta_fd_powerpt5=Delta_fd_lenient,Delta_fd_powerpt5_bf=Delta_fd_lenient_bf,
+                           Delta_fd_powerpt2=Delta_fd_strict,Delta_fd_powerpt2_bf=Delta_fd_strict_bf,Delta_ciric=Delta_rmsd,
+                           Delta_ciric_bf=Delta_rmsd_bf,Mean_FD=Mean_FD,Mean_RMSD=Mean_RMSD,Propfd02=propfd02,Propfd05=propfd05,PropRMSD025=propRMSD025)
   return(Delta_outcome)
 }
 
+#########
 
+path="/data/home4/risk_share/ImproveFConnASD/ABIDE/"
 
+subj_List <- read.table(file = paste0(path,"Liangkang/data/subj_List_JL.txt"),header = FALSE,sep = "",stringsAsFactors = FALSE)
 
+## i=1
+i=1
 
+SITE_ID=subj_List$V1[[i]]
+SUB_ID=subj_List$V2[[i]]
 
-#########################
-
-
-ABIDEI_KKI=50772:50826
-ABIDEI_NYU=c(50952:50962,50964:51003,51006:51021,51023:51030,51032:51036,51038:51042,51044:51091,51093:51131,51146:51156,51159)
-
-ABIDEII_KKI=c(29273:29405,29407:29425,29427:29485)
-ABIDEII_NYU_1=29177:29254
-ABIDEII_NYU_2=29150:29176
-
-
-#source.1=strsplit(source,split = "_")[[1]][2]
-
-
-
-path="/data/home4/risk_share/ImproveFConnASD/ABIDE/fmriprep_preprocessed/"
-
-#### get the SUB_ID
-
-if(source=="ABIDEI-KKI"){
-  SUB_ID=ABIDEI_KKI
+if(SITE_ID=="ABIDEI-KKI"){
   tr=2.5
-  }else if(source=="ABIDEI-NYU"){
-  SUB_ID=ABIDEI_NYU
+}else if(SITE_ID=="ABIDEI-NYU"){
   tr=2
-  }else if(source=="ABIDEII-KKI"){
-  SUB_ID=ABIDEII_KKI
+}else if(SITE_ID=="ABIDEII-KKI"){
   tr=2.5
-  }else if(source=="ABIDEII-NYU_1"){
-  SUB_ID=ABIDEII_NYU_1
+}else if(SITE_ID=="ABIDEII-NYU_1"){
   tr=2
-  }else if(source=="ABIDEII-NYU_2"){
-  SUB_ID=ABIDEII_NYU_2
-  tr=2
-  }
+}else if(SITE_ID=="ABIDEII-NYU_2"){
+  tr=2}
 
-Delta_outcome=Get_Delta(SUB_ID = SUB_ID,source = source,path = path,tr=tr)
+Delta_Outcome=Get_Delta(SITE_ID = SITE_ID,SUB_ID = SUB_ID,path=path,tr=tr)
 
 
-save(Delta_outcome,file =paste0("/data/home4/risk_share/ImproveFConnASD/ABIDE/Liangkang/outcome/sub_delta/",source,"_scrub_outcome.Rdata") )
+for (i in 2:nrow(subj_List)) {
+  try({
+    SITE_ID=subj_List$V1[[i]]
+  SUB_ID=subj_List$V2[[i]]
+  
+  if(SITE_ID=="ABIDEI-KKI"){
+    tr=2.5
+  }else if(SITE_ID=="ABIDEI-NYU"){
+    tr=2
+  }else if(SITE_ID=="ABIDEII-KKI"){
+    tr=2.5
+  }else if(SITE_ID=="ABIDEII-NYU_1"){
+    tr=2
+  }else if(SITE_ID=="ABIDEII-NYU_2"){
+    tr=2}
+  
+  Delta_outcome=Get_Delta(SITE_ID = SITE_ID,SUB_ID = SUB_ID,path=path,tr=tr)
+  Delta_Outcome=rbind(Delta_Outcome,Delta_outcome)
+  
+  })
+    
+}
+
+
+save(Delta_Outcome,file =paste0("/data/home4/risk_share/ImproveFConnASD/ABIDE/Liangkang/outcome/sub_delta/Delta_Outcome.Rdata") )
 
